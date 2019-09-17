a3ef44f9b35ad795b73fd90ffbf33f8f4773d168
gtplesko <gtplesko@gmail.com>
2019-09-16 22:59:51 -0700

    Fixed edge error, added icons

--- mechanics/paa.js
+++ mechanics/paa.js
@@ -11,16 +11,16 @@ let toolBarWidth = 200;
 let alignH = true;
 let prvX,prvY = -1;
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight).canvas;
+  canvas = createCanvas(windowWidth-toolBarWidth, windowHeight).canvas;
   if(xRes < yRes){
     alignH = false;
   }
-  pixelSize = (alignH ? (windowWidth-toolBarWidth)/xRes : (windowHeight)/yRes);
+  pixelSize = (alignH ? (width)/xRes : (windowHeight)/yRes);
   noStroke();
 }
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  pixelSize = (alignH ? (windowWidth-toolBarWidth)/xRes : (windowHeight)/yRes);
+  resizeCanvas(windowWidth-toolBarWidth, windowHeight);
+  pixelSize = (alignH ? (windowWidth)/xRes : (windowHeight)/yRes);
 }
 function clearCanvas(){
   if(confirm('Clear the canvas?')){
@@ -32,7 +32,7 @@ function clearCanvas(){
 function saveCanvas(){
   saveCanvas(canvas, 'pixels','png');
 }
-function pencilTool(){
+function pencilTool(draw){
   let x = mouseX/pixelSize>>0;
   let y = mouseY/pixelSize>>0;
   if(mouseX < 0 || mouseX > xRes * pixelSize || mouseY < 0 || mouseY > yRes * pixelSize || prvX == -1 || prvY == -1){
@@ -41,16 +41,16 @@ function pencilTool(){
   }
   let xDif = prvX-x;
   let yDif = prvY-y;
-  if(x < xRes && y < yRes && x >= 0 && y >= 0 && prvX < xRes && prvY < yRes && prvX >= 0 && prvY >= 0 && (xDif > 1 || yDif > 1 || xDif < -1 || yDif < -1)){
-    intLine(x,y,prvX,prvY);
-  }
+
   if(x < xRes && y < yRes && x>=0 && y>=0){
     currentMatrix[(x)+(y*xRes)] = "#000";
-
+    if(x < xRes && y < yRes && x >= 0 && y >= 0 && prvX < xRes && prvY < yRes && prvX >= 0 && prvY >= 0 && (xDif > 1 || yDif > 1 || xDif < -1 || yDif < -1)){
+      intLine(x,y,prvX,prvY);
+    }
     if(xsym){
-      xs = xsym? xRes - x: x;
+      xs = xsym? xRes-1 - x: x;
       ys = y;
-      let pxs = xsym? xRes - prvX: prvX;
+      let pxs = xsym? xRes-1 - prvX: prvX;
       let pys = prvY;
       currentMatrix[(xs)+(ys*xRes)] = "#000";
       if(xs < xRes && ys < yRes && xs >= 0 && ys >= 0 && pxs < xRes && pys < yRes && pxs >= 0 && pys >= 0 && (xDif > 1 || yDif > 1 || xDif < -1 || yDif < -1)){
@@ -59,19 +59,19 @@ function pencilTool(){
     }
     if(ysym){
       xs = x;
-      ys = ysym? yRes - y: y;
+      ys = ysym? yRes-1 - y: y;
       let pxs = prvX;
-      let pys = ysym? yRes - prvY: prvY;
+      let pys = ysym? yRes-1 - prvY: prvY;
       currentMatrix[(xs)+(ys*xRes)] = "#000";
       if(xs < xRes && ys < yRes && xs >= 0 && ys >= 0 && pxs < xRes && pys < yRes && pxs >= 0 && pys >= 0 && (xDif > 1 || yDif > 1 || xDif < -1 || yDif < -1)){
         intLine(xs,ys,pxs,pys);
       }
     }
     if(xsym&&ysym){
-      xs = xRes - x;
-      ys = yRes - y;
-      let pxs = xRes - prvX;
-      let pys = yRes - prvY;
+      xs = xRes-1 - x;
+      ys = yRes-1 - y;
+      let pxs = xRes-1 - prvX;
+      let pys = yRes-1 - prvY;
       currentMatrix[(xs)+(ys*xRes)] = "#000";
       if(xs < xRes && ys < yRes && xs >= 0 && ys >= 0 && pxs < xRes && pys < yRes && pxs >= 0 && pys >= 0 && (xDif > 1 || yDif > 1 || xDif < -1 || yDif < -1)){
         intLine(xs,ys,pxs,pys);
@@ -173,14 +173,12 @@ function draw() {
   if(grid){
 
     stroke(255,0,0);
-    // if(xsym){
-    //   line(xRes*pixelSize/2, 0, xRes*pixelSize/2, yRes*pixelSize);
-    //   line(xRes*pixelSize/2+pixelSize, 0, xRes*pixelSize/2+pixelSize, yRes*pixelSize);
-    // }
-    // if(ysym){
-    //   line(0, yRes*pixelSize/2, xRes*pixelSize, yRes*pixelSize/2);
-    //   line(0, yRes*pixelSize/2+pixelSize, xRes*pixelSize, yRes*pixelSize/2+pixelSize);
-    // }
+    if(xsym){
+      line(xRes*pixelSize/2, 0, xRes*pixelSize/2, yRes*pixelSize);
+    }
+    if(ysym){
+      line(0, yRes*pixelSize/2, xRes*pixelSize, yRes*pixelSize/2);
+    }
     stroke(0);
     line(1,0,pixelSize*xRes,0);
     line(pixelSize*xRes,0,pixelSize*xRes,pixelSize*yRes);
